9d2b84b9b14c766f1dcb0cc98286bef13b3e2563
Ahmed Ibrahim <ahmedibrahim@192.168.192.128>
2019-07-06 09:43:17 -0700

    new proj

---
+++ Instagram/InformationCell.swift
@@ -0,0 +1,24 @@
+//
+//  InformationCell.swift
+//  Instagram
+//
+//  Created by Ahmed Ibrahim on 7/5/19.
+//  Copyright © 2019 Ahmed Ibrahim. All rights reserved.
+//
+
+import UIKit
+
+class InformationCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Instagram/StoriesCell.swift
@@ -0,0 +1,24 @@
+//
+//  StoriesCell.swift
+//  Instagram
+//
+//  Created by Ahmed Ibrahim on 7/5/19.
+//  Copyright © 2019 Ahmed Ibrahim. All rights reserved.
+//
+
+import UIKit
+
+class StoriesCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
